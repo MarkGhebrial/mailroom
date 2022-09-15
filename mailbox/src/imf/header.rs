dbf31be01cb6bbb89d48c300f7408f2cec7d984b
Mark Ghebrial <mghebrialjr@icloud.com>
2022-09-14 20:36:52 -0700

    Parse RFC 5322 header field names

--- mailbox/src/imf/header.rs
+++ mailbox/src/imf/header.rs
@@ -1,17 +1,65 @@
 /// Represents a header in an Internet Message Format message
 pub struct ImfHeader {
-    name: HeaderName,
-    body: HeaderBody
+    pub name: HeaderName,
+    pub body: HeaderBody
 }
 
+/// Enumerates all the header names specified in RFC 5322
 pub enum HeaderName {
-    Subject,
-    To,
+    Date,
     From,
-    //TODO
+    Sender,
+    ReplyTo,
+    To,
+    Cc,
+    Bcc,
+    MessageID,
+    InReplyTo,
+    References,
+    Subject,
+    Comments,
+    Keywords,
+    ResentDate,
+    ResentFrom,
+    ResentSender,
+    ResentTo,
+    ResentCc,
+    ResentBcc,
+    ResentMessageID,
+    ReturnPath,
+    Other(String)
+}
+
+impl From<&str> for HeaderName {
+    fn from(string: &str) -> Self {
+        use HeaderName::*;
+        match string {
+            "Date" => Date,
+            "From" => From,
+            "Sender" => Sender,
+            "Reply-To" => ReplyTo,
+            "To" => To,
+            "Cc" => Cc,
+            "Bcc" => Bcc,
+            "Message-ID" => MessageID,
+            "In-Reply-To" => InReplyTo,
+            "References" => References,
+            "Subject" => Subject,
+            "Comments" => Comments,
+            "Keywords" => Keywords,
+            "Resent-Date" => ResentDate,
+            "Resent-From" => ResentFrom,
+            "Resent-Sender" => ResentSender,
+            "Resent-To" => ResentTo,
+            "Resent-Cc" => ResentCc,
+            "Resent-Bcc" => ResentBcc,
+            "Resent-Message-ID" => ResentMessageID,
+            "Return-Path" => ReturnPath,
+            s => Other(s.to_owned())
+        }
+    }
 }
 
 pub enum HeaderBody {
     Unstructured(String),
-    
 }
