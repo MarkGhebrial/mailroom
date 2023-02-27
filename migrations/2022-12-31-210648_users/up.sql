c783e3643cdbffdd7770a5f12e3a34486a533671
MarkGhebrial <mghebrialjr@icloud.com>
2023-02-27 09:35:42 -0800

    Revert "Setup diesel"
    This reverts commit 899968dcebf59053c1ba04d51196900477b1ee7a.

--- migrations/2022-12-31-210648_users/up.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (
-  email_address TEXT PRIMARY KEY,
-  mailbox TEXT ARRAY
-)
--- migrations/2022-12-31-213415_mail/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE mail (
-  message_id TEXT PRIMARY KEY,
-  recipients TEXT ARRAY,
-  sent_by TEXT
-
-  --body TEXT NOT NULL,
-  --published BOOLEAN NOT NULL DEFAULT FALSE
-)
